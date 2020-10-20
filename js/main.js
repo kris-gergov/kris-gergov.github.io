21f10b0d5b8cd4862f655f9b2efaea9715156a7a
kris-gergov <krisgergov@gmail.com>
2020-10-20 15:17:02 +0100

    Fixed viewport while scrolling on mobile

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+$(document).ready(function() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        var viewportHeight = $('.header').outerHeight();
+        $('.header').css({ height: viewportHeight });
+      }
+});
 
 $( ".navigation__link" ).on( "click", function() {
     $( "#navi-toggle" ).prop( "checked", false );

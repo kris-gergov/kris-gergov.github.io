42d3f994a27550d296ea30533314075d9daf0e27
kris-gergov <krisgergov@gmail.com>
2020-10-20 18:38:04 +0100

    Scrolling changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+function greedyJumbotron() {
+    var HEIGHT_CHANGE_TOLERANCE = 100; // Approximately URL bar height in Chrome on tablet
+
+    var jumbotron = $(this);
+    var viewportHeight = $(window).height();
+
+    $(window).resize(function () {
+        if (Math.abs(viewportHeight - $(window).height()) > HEIGHT_CHANGE_TOLERANCE) {
+            viewportHeight = $(window).height();
+            update();
+        }
+    });
+
+    function update() {
+        jumbotron.css('height', viewportHeight + 'px');
+    }
+
+    update();
+}
+
+$('.header').each(greedyJumbotron);
+
 $( ".navigation__link" ).on( "click", function() {
     $( "#navi-toggle" ).prop( "checked", false );
 });
